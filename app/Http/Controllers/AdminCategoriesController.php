c453dc09a825296627dc0153d23a518472966254
Miguel Angelo Pacifico <miguel.pacifico@gmail.com>
2015-04-29 19:55:38 -0300

    Criação dos Controllers

---
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -0,0 +1,12 @@
+<?php namespace CodeCommerce\Http\Controllers;
+
+use CodeCommerce\Http\Requests;
+use CodeCommerce\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class AdminCategoriesController extends Controller {
+
+	//
+
+}
