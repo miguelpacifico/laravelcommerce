3848fb9c57ab71feca0cae0ebd8a017e47bcd44e
Miguel Angelo Pacifico <miguel.pacifico@gmail.com>
2015-04-30 18:23:20 -0300

    Ajustando Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,13 +11,62 @@
 |
 */
 
+Route::pattern('id','[0-9]+');
+
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::get('admin/categories', 'AdminCategoriesController@index');
+Route::group(['prefix' => 'admin'], function()
+{
 
-Route::get('admin/products', 'AdminProductsController@index');
+    Route::group(['prefix' => 'categories'], function()
+    {
+        Route::get('list', [
+            'as' => 'categories.list', 'uses' => 'AdminCategoriesController@index'
+        ]);
+
+        Route::get('add', [
+            'as' => 'categories.add', 'uses' => 'AdminCategoriesController@index'
+        ]);
+
+        Route::get('show/{id}', [
+            'as' => 'categories.show', 'uses' => 'AdminCategoriesController@index'
+        ]);
+
+        Route::get('edit/{id}', [
+            'as' => 'categories.edit', 'uses' => 'AdminCategoriesController@index'
+        ]);
+
+    });
+
+    Route::group(['prefix' => 'products'], function()
+    {
+        Route::get('list', [
+            'as' => 'products.list', 'uses' => 'AdminProductsController@index'
+        ]);
+
+        Route::get('add', [
+            'as' => 'products.add', 'uses' => 'AdminProductsController@index'
+        ]);
+
+        Route::get('show/{id}', [
+            'as' => 'products.show', 'uses' => 'AdminProductsController@index'
+        ]);
+
+        Route::get('edit/{id}', [
+            'as' => 'products.edit', 'uses' => 'AdminProductsController@index'
+        ]);
+
+    });
+
+
+    //Route::get('categories', 'AdminCategoriesController@index');
+
+    //Route::get('products', 'AdminProductsController@index');
+});
+
+
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
